2d5e2981f40c7d2cfc0df1851ceb9cbcb190e2f2
Hiruni Perera <hiruni.p@eyepax.com>
2022-09-09 15:37:42 +0530

    complete login and signup routes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,23 @@
 import React from "react";
+
+//antd components
 import { PageHeader, Button } from 'antd';
+
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <PageHeader
     title="Salon Prauge Admin"
     extra={[
-        <Button key="2">Login</Button>,
-        <Button key="1" type="primary">
-          Signup
-        </Button>,
+        (
+        <Link to="/login">
+            <Button key="2">Login</Button>
+        </Link>)
+        ,
+        (<Link to="/signup">
+            <Button key="1" type="primary">Signup</Button>
+        </Link>)
       ]}
   />
   );
