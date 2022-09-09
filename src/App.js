82b0ec0034a2ba01185f8a6e574ec764819948e4
Hiruni Perera <hiruni.p@eyepax.com>
2022-09-09 15:26:52 +0530

    routing for login and signup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+//components
 import Navbar from "./components/Navbar"
+
+//pages
+import Home from "./pages/home"
+import Login from "./pages/login"
+import Signup from "./pages/signup"
+
+//styles
 import "antd/dist/antd.min.css"
+
+//routing
+import {
+  Route,
+  Routes
+} from "react-router-dom";
 
 function App() {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </>
+    
   );
 }
 
