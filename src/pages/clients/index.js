91096387749224500ae8ab67c5cb4658911059db
Hiruni Perera <hiruni.p@eyepax.com>
2022-09-11 15:07:03 +0530

    clients ui

--- src/pages/clients/index.js
+++ src/pages/clients/index.js
@@ -1,11 +1,57 @@
-import React from 'react'
+import { Space, Table } from 'antd';
+import React from 'react';
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    render: (text) => <div>{text}</div>,
+  },
+  {
+    title: 'Email',
+    dataIndex: 'email',
+    key: 'email',
+  },
+  {
+    title: 'Phone',
+    dataIndex: 'phone',
+    key: 'phone',
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (_, record) => (
+      <Space size="middle">
+        <button>Edit</button>
+        <button>Delete</button>
+      </Space>
+    ),
+  },
+];
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer'],
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser'],
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+];
 
-const Clients = () => {
-  return (
-    <div>
-      Clients
-    </div>
-  )
-}
+const Clients = () => <Table columns={columns} dataSource={data} />;
 
-export default Clients
+export default Clients;
