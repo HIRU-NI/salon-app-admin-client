c2a25f638ae8c8007f339af144b61e58a1989fcb
Hiruni Perera <hiruni.p@eyepax.com>
2022-09-14 08:37:38 +0530

    add live server base url

--- src/features/axios.js
+++ src/features/axios.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 
-const instance = axios.create({baseURL: 'http://localhost:8080'});
+const instance = axios.create({baseURL: 'https://salon-prauge-admin-server.herokuapp.com/'});
 export default instance
