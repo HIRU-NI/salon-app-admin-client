27035faccdcaffffb2cca19ee88a32a46c27ef26
Hiruni Perera <hiruni.p@eyepax.com>
2022-09-16 12:57:10 +0530

    fix navigation to login error

--- src/App.js
+++ src/App.js
@@ -1,75 +1,72 @@
 //components
-import Navbar from "./components/Navbar"
-import Sidebar from './components/Sidebar'
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 
 //pages
-import Home from "./pages/home"
-import Login from "./pages/login"
-import Signup from "./pages/signup"
-import Clients from "./pages/clients"
-import Reservations from "./pages/reservations"
-import Calendar from "./pages/calendar"
-import Admins from "./pages/admin"
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Signup from "./pages/signup";
+import Clients from "./pages/clients";
+import Reservations from "./pages/reservations";
+import Calendar from "./pages/calendar";
+import Admins from "./pages/admin";
 
 //styles
-import "antd/dist/antd.min.css"
+import "antd/dist/antd.min.css";
 
 //routing
-import {
-  Route,
-  Routes
-} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 //toastify (alerts)
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 //antd components
-import { Layout } from "antd"
+import { Layout } from "antd";
 
-import { useSelector } from "react-redux"
+import { useSelector } from "react-redux";
 
-const { Sider, Content } = Layout
-
-
+const { Sider, Content } = Layout;
 
 function App() {
-  const { user } = useSelector( state => state.auth )
+  const { user } = useSelector((state) => state.auth);
   return (
-    <Layout style={{minHeight: "100vh"}}>
+    <Layout style={{ minHeight: "100vh" }}>
       <Navbar />
       <Layout>
-        {
-          user ? (
-            <Sider style={{background:"#fff"}}>
-              <Sidebar />
-            </Sider>
-          ) : (<></>)
-        }
+        {user ? (
+          <Sider style={{ background: "#fff" }}>
+            <Sidebar />
+          </Sider>
+        ) : (
+          <></>
+        )}
         <Layout>
           <Content
             style={{
-              margin: '24px 16px 0',
-          }}>
-              <div
-                style={{
-                  padding: 24,
-                  minHeight: 360,
-                }}>
-                <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/login" element={<Login />} />
-                  <Route path="/signup" element={<Signup />} />
-                  <Route path="/clients" element={<Clients />} />
-                  <Route path="/reservations" element={<Reservations />} />
-                  <Route path="/calendar" element={<Calendar />} />
-                  <Route path="/admin" element={<Admins />} />
-                </Routes>
-              </div>
+              margin: "24px 16px 0",
+            }}
+          >
+            <div
+              style={{
+                padding: 24,
+                minHeight: 360,
+              }}
+            >
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/clients" element={<Clients />} />
+                <Route path="/reservations" element={<Reservations />} />
+                <Route path="/calendar" element={<Calendar />} />
+                <Route path="/admin" element={<Admins />} />
+              </Routes>
+            </div>
           </Content>
         </Layout>
       </Layout>
-      
+
       <ToastContainer />
     </Layout>
   );
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,131 +1,128 @@
-import { React, useEffect} from 'react';
+import { React, useEffect } from "react";
 
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 //redux
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from "react-redux";
 
-import { Col, Row, Card } from 'antd';
+import { Col, Row, Card } from "antd";
 
-import { Pie, Bar } from '@ant-design/plots';
-import { getAllReservations, reset } from '../../features/reservations/reservationSlice';
-import { toast } from 'react-toastify';
+import { Pie, Bar } from "@ant-design/plots";
+import {
+  getAllReservations,
+  reset,
+} from "../../features/reservations/reservationSlice";
+import { toast } from "react-toastify";
 
 const getPieChartConfiguration = (reservations) => {
   const pieChartData = [
     {
-      type: 'Scheduled',
-      value: reservations.filter(reservation=> reservation.isComplete === false).length,
+      type: "Scheduled",
+      value: reservations.filter(
+        (reservation) => reservation.isComplete === false
+      ).length,
     },
     {
-      type: 'Completed',
-      value: reservations.filter(reservation=> reservation.isComplete === true).length,
+      type: "Completed",
+      value: reservations.filter(
+        (reservation) => reservation.isComplete === true
+      ).length,
     },
-  ]
+  ];
 
   const configPie = {
     appendPadding: 10,
     data: pieChartData,
-    angleField: 'value',
-    colorField: 'type',
+    angleField: "value",
+    colorField: "type",
     radius: 0.9,
     label: {
-      type: 'inner',
-      offset: '-30%',
+      type: "inner",
+      offset: "-30%",
       content: ({ percent }) => `${(percent * 100).toFixed(0)}%`,
       style: {
         fontSize: 14,
-        textAlign: 'center',
+        textAlign: "center",
       },
     },
     interactions: [
       {
-        type: 'element-active',
+        type: "element-active",
       },
     ],
   };
-  
-  return configPie
-} 
 
-  const dataBar = [
-    {
-        stylist: 'stylist 1',
-        value: 1,
-    },
-    {
-        stylist: 'stylist 2',
-        value: 4,
-    },
-    {
-        stylist: 'stylist 3',
-        value: 5,
-    },
-    {
-        stylist: 'stylist 4',
-        value: 2,
-    },
-    {
-        stylist: 'stylist 5',
-        value: 8,
-    },
-  ];
+  return configPie;
+};
+const dataBar = [
+  {
+    stylist: "stylist 1",
+    value: 1,
+  },
+  {
+    stylist: "stylist 2",
+    value: 4,
+  },
+  {
+    stylist: "stylist 3",
+    value: 5,
+  },
+  {
+    stylist: "stylist 4",
+    value: 2,
+  },
+  {
+    stylist: "stylist 5",
+    value: 8,
+  },
+];
 
-  
-  const configBar = {
-    data: dataBar,
-    xField: 'value',
-    yField: 'stylist',
-    seriesField: 'stylist',
-    legend: {
-      position: 'top-left',
-    },
-  };
+const configBar = {
+  data: dataBar,
+  xField: "value",
+  yField: "stylist",
+  seriesField: "stylist",
+  legend: {
+    position: "top-left",
+  },
+};
 
 const Home = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
+  const { user } = useSelector((state) => state.auth);
+  const { reservations, isError, message } = useSelector(
+    (state) => state.reservation
+  );
 
-    const { user } = useSelector( state => state.auth)
-    const { reservations , isError,  message } = useSelector(state => state.reservation)
+  useEffect(() => {
+    if (isError) toast.error(message);
+    if (!user) navigate("/login");
 
-    useEffect(() => {
-      if(isError) toast.error(message)
-      if(!user) navigate('/login')
-    
-      dispatch(getAllReservations())
-  
-      return () => {
-        dispatch(reset())
-      }
+    if (user) dispatch(getAllReservations());
 
-    }, [user, navigate, dispatch, isError, message])
+    return () => {
+      dispatch(reset());
+    };
+  }, [user, navigate, dispatch, isError, message]);
 
-    return (
-        <div>
-            <Row gutter={16}>
-                <Col span={12}>
-                    <Card
-                       
-                        title="Reservation Status"
-                    >
-                        <Pie {...getPieChartConfiguration(reservations)} />
-                    </Card>  
-                </Col>
-                <Col span={12}>
-                    <Card
-                       
-                       title="Stylist Allocations"
-                    >
-                        <Bar {...configBar}/>
-                   </Card>  
-                   
-                </Col>
-    </Row>
-            
-        </div>
-    )
-}
+  return (
+    <div>
+      <Row gutter={16}>
+        <Col span={12}>
+          <Card title="Reservation Status">
+            <Pie {...getPieChartConfiguration(reservations)} />
+          </Card>
+        </Col>
+        <Col span={12}>
+          <Card title="Stylist Allocations">
+            <Bar {...configBar} />
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
+};
 
 export default Home;
