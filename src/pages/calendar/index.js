7bf496875ac1a993ab5ac2a678da0d262de2b474
Hiruni Perera <hiruni.p@eyepax.com>
2022-09-21 10:23:45 +0530

    calander drag n drop: in progress

--- src/pages/calendar/index.js
+++ src/pages/calendar/index.js
@@ -9,9 +9,16 @@ import { toast } from "react-toastify";
 import {
   getAllReservations,
   reset,
+  updateReservation,
 } from "../../features/reservations/reservationSlice";
 import moment from "moment";
-import { getAllServices, reset as resetServices } from "../../features/services/serviceSlice";
+import {
+  getAllServices,
+  reset as resetServices,
+} from "../../features/services/serviceSlice";
+
+//drag n drop
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 const getListData = (value, reservations, services) => {
   let listData;
@@ -22,11 +29,12 @@ const getListData = (value, reservations, services) => {
     );
     if (moment(reservation.date).isSame(value, "day") && service) {
       return {
-        content:  service.name,
+        content: service.name,
         type: reservation.isComplete ? "green" : "cyan",
+        id: reservation._id
       };
     }
-    return null
+    return null;
   });
   return listData || [];
 };
@@ -46,30 +54,104 @@ const ReservationsCalendar = () => {
     if (!user) navigate("/login");
 
     dispatch(getAllReservations());
-    dispatch(getAllServices())
+    dispatch(getAllServices());
 
     return () => {
       dispatch(reset());
-      dispatch(resetServices())
+      dispatch(resetServices());
     };
   }, [user, navigate, isError, message, dispatch]);
 
   const dateCellRender = (value) => {
     const listData = getListData(value, reservations, services);
-    
+
     return (
-      <>
-        {listData.map((item) => (
-          item ? <Badge key={item.content} color={item.type} text={item.content} /> : <></>
-        ))}
-      </>
+      <Droppable droppableId={value.toString()} key={value.toString()} index={value.toString()}>
+        {(provided) => {
+          let index = 0;
+          return (
+            <div
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+              key={value.toString()}
+            >
+              {listData.filter(data => data != null).map((item) => {
+                if (item) {
+                 
+                  index++;
+                  return (
+                    <Draggable
+                      draggableId={item.id}
+                      index={index+1}
+                      key={item.id}
+                    >
+                      {(provided) => {
+                        return (
+                          <div
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                            ref={provided.innerRef}
+                          >
+                            <Badge
+                              key={index}
+                              color={item.type}
+                              text={item.content}
+                            />
+                          </div>
+                        );
+                      }}
+                    </Draggable>
+                  );
+                }
+                return (<></>)
+              })}
+              {provided.placeholder}
+            </div>
+          );
+        }}
+      </Droppable>
     );
   };
 
+  const onDragStart = (result) => {};
+
+  const onDragUpdate = () => {};
+
+  const onDragEnd = (result) => {
+    const { destination, draggableId, source} = result
+
+    const reservation = reservations.find(reservation => reservation._id === draggableId)
+
+    console.log(destination)
+
+    if (
+      destination.droppableId === source.droppableId 
+    ) {
+      return;
+    }
+    
+
+    dispatch(updateReservation({
+      id: reservation._id,
+      reservation: {
+        client: reservation.client,
+        service: reservation.service,
+        stylist: reservation.stylist,
+        date: moment(destination.droppableId),
+        isComplete: reservation.isComplete,
+      },
+    }))
+  };
 
   return (
     <Card>
-      <Calendar dateCellRender={dateCellRender} />
+      <DragDropContext
+        onDragStart={onDragStart}
+        onDragUpdate={onDragUpdate}
+        onDragEnd={onDragEnd}
+      >
+        <Calendar dateCellRender={dateCellRender} />
+      </DragDropContext>
     </Card>
   );
 };
